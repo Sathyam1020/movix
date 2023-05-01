1a6266b5c195df3443821cd735c3ce40a2fbeae1
sathyam1020 <sathyamrock55@gmail.com>
2023-05-01 23:33:11 +0530

    Added folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import { useEffect } from 'react';
 import { fetchDataFromApi } from './utils/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { getApiConfiguration } from './redux/Slices/HomeSlice';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//* PAGES
+import Footer from './components/footer/Footer';
+import Header from './components/header/header'
+import Home from './pages/home/Home';
+import Details from './pages/details/Details';
+import SearchResults from './pages/searchResults/SearchResults';
+import Explore from './pages/explore/Explore';
+import Four04 from './pages/404/Four04';
+
 
 function App() {
 
